94d5c4105c67b822d44884a219475f0302324e50
Mary Tyutyunnyk <mary.tyutyunnyk@gmail.com>
2019-08-15 20:56:12 +0300

    Changes in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,13 +13,6 @@ const cssmin = require('gulp-cssmin');
 const autoprefixer = require('gulp-autoprefixer');
 const gcmq = require('gulp-group-css-media-queries');
 
-// JS
-const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat'); // For files concatination
-
-// Images
-const imagemin = require('gulp-imagemin');
-
 gulp.task('clean', () =>
  	gulp.src('./dist', {
       read: false
@@ -31,16 +24,6 @@ gulp.task('html', () => {
 	gulp.src('./src/index.html')
 		.pipe(gulp.dest('./dist'))
 });
-
-// gulp.task('js', () =>
-// 	gulp.src
-// 	('./src/js/script.js')
-// 		.pipe(plumber())
-// 		.pipe(sourcemaps.init())
-// 		.pipe(concat('script.min.js'))
-// 		.pipe(sourcemaps.write())
-// 		.pipe(gulp.dest('./dist/js')),
-// );
 
 gulp.task('scss', () =>
 	gulp.src('./src/scss/main.scss')
@@ -62,7 +45,6 @@ gulp.task('scss', () =>
 
 gulp.task('img', () => {
 	gulp.src('./src/img/*')
-		// .pipe(imagemin())
 		.pipe(gulp.dest('dist/img'))
 });
 
@@ -80,7 +62,6 @@ gulp.task('dev', ['build'], () => {
 	});
 	gulp.watch('./src/scss/**/*.scss', ['scss']).on('change', browserSync.reload);
 	gulp.watch('./src/img/**/*', ['img']).on('change', browserSync.reload);
-	gulp.watch('./src/js/script.js', ['js']).on('change', browserSync.reload);
 	gulp.watch('./src/index.html').on('change', browserSync.reload);
 });
 
